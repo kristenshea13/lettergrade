b5af7e53c4d513638457df714f2a356fcbc3ca3b
Kristen Shea <kstar61@live.com>
2018-08-29 17:39:05 -0500

    Add project files.

---
+++ letter grades/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace letter_grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
