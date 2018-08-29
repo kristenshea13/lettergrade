9a751d206cfd827b543345ddf8fdbf52f752c510
Kristen Shea <kstar61@live.com>
2018-08-29 17:50:13 -0500

    determine letter grade from user entered exam score

--- letter grades/Program.cs
+++ letter grades/Program.cs
@@ -6,7 +6,36 @@ namespace letter_grades
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //Write a method which is given an exam score, and it returns the corresponding letter grade as a 
+            //    string according to this scheme: >= 90 A, [80-90) B, [70-80) C, [60-70) D, < 60 F
+
+            Console.WriteLine("What grade do you have?");
+            Console.Write("Please enter your test score: ");
+            int testScore = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Your grade is: {0}", LetterGrade(testScore));
+            Console.ReadLine();
+
+
         }
+
+        private static string LetterGrade(int testScore)
+        {
+            if (testScore >= 90)
+                return "A";
+            else if (testScore >= 80 && testScore <= 90)
+                return "B";
+            else if (testScore >= 70 && testScore <= 80)
+                return "C";
+            else if (testScore >= 60 && testScore <= 70)
+                return "D";
+            else
+                return "F";
+
+        }
+
+
+
     }
 }
